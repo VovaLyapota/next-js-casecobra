d5bab114358289e22a0d3872c26c5f90c8f08f2f
VovaLyapota <rimidalvatopayl@gmail.com>
2024-06-12 12:04:55 +0300

    finish stripe webhook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,5 +1,7 @@
+import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 export async function POST(req: Request) {
@@ -35,6 +37,44 @@ export async function POST(req: Request) {
 
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
+
+      await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state!,
+            },
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state!,
+            },
+          },
+        },
+      });
     }
-  } catch (err) {}
+
+    return NextResponse.json({ result: event, ok: true });
+  } catch (err) {
+    console.error(err);
+
+    return NextResponse.json(
+      { message: "Something went wrong", ok: false },
+      { status: 500 }
+    );
+  }
 }
